0173ee431181cff6d47ad3abbbafc3c92f3f89af
zaruk <zaruk@DESKTOP-0277CMG>
2021-01-21 12:18:33 +0100

    commit inicial

---
+++ src/ejercicionivel1/EjercicioNivel1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicionivel1;
+
+/**
+ *
+ * @author zaruk
+ */
+public class EjercicioNivel1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
